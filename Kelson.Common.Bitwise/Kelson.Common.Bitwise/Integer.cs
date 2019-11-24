39680f40473e88fdc912abc206cf40c171be8d48
Kelson Ball <kelson.j.ball@gmail.com>
2019-11-24 10:50:04 -0800

    Change method sig style

--- Kelson.Common.Bitwise/Kelson.Common.Bitwise/Integer.cs
+++ Kelson.Common.Bitwise/Kelson.Common.Bitwise/Integer.cs
@@ -40,13 +40,13 @@
         /// <param name="data"></param>
         /// <param name="byte"></param>
         /// <returns></returns>
-        public static byte Byte(this int data, Bytes @byte)
-            => (byte)((data & 0xFF << (int)@byte) >> (int)@byte);
+        public static byte Byte(this int data, Bytes @byte) => 
+            (byte)((data & 0xFF << (int)@byte) >> (int)@byte);
 
-        public static int Pack(byte first, byte second, byte third, byte fourth)
-            => first << (int)Bytes.First | second << (int)Bytes.Second | third << (int)Bytes.Third | fourth << (int)Bytes.Fourth;
+        public static int Pack(byte first, byte second, byte third, byte fourth) => 
+            first << (int)Bytes.First | second << (int)Bytes.Second | third << (int)Bytes.Third | fourth << (int)Bytes.Fourth;
         
-        public static (byte, byte, byte, byte) Unpack(this int data)
-            => (data.Byte(Bytes.First), data.Byte(Bytes.Second), data.Byte(Bytes.Third), data.Byte(Bytes.Fourth));
+        public static (byte, byte, byte, byte) Unpack(this int data) => 
+            (data.Byte(Bytes.First), data.Byte(Bytes.Second), data.Byte(Bytes.Third), data.Byte(Bytes.Fourth));
     }
 }
